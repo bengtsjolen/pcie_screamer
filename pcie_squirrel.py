84651d8f5a0fa0258ce0dc754ce73e30836953a0
Bengt Sjölén <bengt@automata.se>
2026-03-12 17:36:18 +0100

    adding squirrel board

---
+++ pcie_squirrel.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# PCIe Screamer Squirrel - LiteX/Migen target
+# Based on enjoy-digital/pcie_screamer by Florent Kermarrec
+# Adapted for Screamer PCIe Squirrel (XC7A35T-FGG484)
+#
+# Usage:
+#   python3 pcie_squirrel.py --build
+#   python3 pcie_squirrel.py --build --load    # JTAG
+#   python3 pcie_squirrel.py --build --flash   # SPI flash
+
+import os
+import argparse
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.build.generic_platform import *
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.interconnect import stream
+from litex.soc.cores.uart import UARTWishboneBridge
+from litex.soc.cores.usb_fifo import phy_description
+
+from litepcie.phy.s7pciephy import S7PCIEPHY
+
+# Import gateware from pcie_screamer repo
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../pcie_screamer"))
+from gateware.usb import USBCore
+from gateware.etherbone import Etherbone
+from gateware.tlp import TLP
+from gateware.msi import MSI
+from gateware.ft601 import FT601Sync
+
+from pcie_squirrel_platform import Platform
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_usb = ClockDomain()
+
+        # sys clock: 100MHz oscillator -> PLL -> sys
+        sys_clk_100 = platform.request("clk100")
+        platform.add_period_constraint(sys_clk_100, 1e9/100e6)
+        self.submodules.pll = pll = S7PLL(speedgrade=-2)
+        pll.register_clkin(sys_clk_100, 100e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+
+        # usb clock: from FT601 chip
+        usb_clk100 = platform.request("usb_fifo_clock")
+        platform.add_period_constraint(usb_clk100, 1e9/100e6)
+        self.comb += self.cd_usb.clk.eq(usb_clk100)
+        self.specials += AsyncResetSynchronizer(self.cd_usb, ResetSignal("pcie"))
+
+# PCIeSquirrel -------------------------------------------------------------------------------------
+
+class PCIeSquirrel(SoCMini):
+    usb_map = {
+        "wishbone": 0,
+        "tlp":      1,
+    }
+
+    def __init__(self, platform, with_loopback=False):
+        sys_clk_freq = int(100e6)
+
+        SoCMini.__init__(self, platform, sys_clk_freq,
+                         ident="PCIe Screamer Squirrel", ident_version=True)
+
+        # CRG
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+
+        # PCIe PHY
+        self.submodules.pcie_phy = S7PCIEPHY(platform, platform.request("pcie_x1"),
+                                              data_width=64, bar0_size=0x40000)
+        self.add_csr("pcie_phy")
+
+        # USB FT601 PHY
+        self.submodules.usb_phy = FT601Sync(platform.request("usb_fifo"), dw=32, timeout=1024)
+
+        if with_loopback:
+            # USB loopback for testing
+            self.submodules.usb_loopback_fifo = stream.SyncFIFO(phy_description(32), 2048)
+            self.comb += [
+                self.usb_phy.source.connect(self.usb_loopback_fifo.sink),
+                self.usb_loopback_fifo.source.connect(self.usb_phy.sink),
+            ]
+        else:
+            # USB Core
+            self.submodules.usb_core = USBCore(self.usb_phy, sys_clk_freq)
+
+            # USB <--> Wishbone
+            self.submodules.etherbone = Etherbone(self.usb_core, self.usb_map["wishbone"])
+            self.bus.add_master(master=self.etherbone.master.bus)
+
+            # USB <--> TLP
+            self.submodules.tlp = TLP(self.usb_core, self.usb_map["tlp"])
+            self.comb += [
+                self.pcie_phy.source.connect(self.tlp.sender.sink),
+                self.tlp.receiver.source.connect(self.pcie_phy.sink),
+            ]
+
+        # MSI
+        self.submodules.msi = MSI()
+        self.comb += self.msi.source.connect(self.pcie_phy.msi)
+        self.add_csr("msi")
+
+        # LEDs
+        usb_counter = Signal(32)
+        self.sync.usb += usb_counter.eq(usb_counter + 1)
+        self.comb += platform.request("user_led", 0).eq(usb_counter[26])
+
+        pcie_counter = Signal(32)
+        self.sync.pcie += pcie_counter.eq(pcie_counter + 1)
+        self.comb += platform.request("user_led", 1).eq(pcie_counter[26])
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="PCIe Screamer Squirrel Gateware")
+    parser.add_argument("--with-loopback", action="store_true", help="Enable USB loopback")
+    parser.add_argument("--build",         action="store_true", help="Build bitstream")
+    parser.add_argument("--load",          action="store_true", help="Load via JTAG")
+    parser.add_argument("--flash",         action="store_true", help="Flash to SPI")
+    args = parser.parse_args()
+
+    platform = Platform()
+    soc      = PCIeSquirrel(platform, with_loopback=args.with_loopback)
+    builder  = Builder(soc, csr_csv="test/csr.csv")
+    builder.build(run=args.build)
+
+    if args.load:
+        prog = platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
+
+    if args.flash:
+        prog = platform.create_programmer()
+        prog.flash(0, os.path.join(builder.gateware_dir, soc.build_name + ".bin"))
+
+if __name__ == "__main__":
+    main()
---
+++ platforms/pcie_squirrel.py
@@ -0,0 +1,64 @@
+# PCIe Screamer Squirrel platform for LiteX/Migen
+# Based on enjoy-digital/pcie_screamer platform
+# Adapted for Screamer PCIe Squirrel (XC7A35T-FGG484)
+
+from migen import *
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform
+
+_io = [
+    # 100 MHz system clock
+    ("clk100", 0, Pins("H4"), IOStandard("LVCMOS33")),
+
+    # LEDs
+    ("user_led", 0, Pins("Y6"),  IOStandard("LVCMOS33")),
+    ("user_led", 1, Pins("AB5"), IOStandard("LVCMOS33")),
+
+    # PCIe x1
+    ("pcie_x1", 0,
+        Subsignal("rst_n", Pins("B13"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F6")),
+        Subsignal("clk_n", Pins("E6")),
+        Subsignal("rx_p",  Pins("B10")),
+        Subsignal("rx_n",  Pins("A10")),
+        Subsignal("tx_p",  Pins("B6")),
+        Subsignal("tx_n",  Pins("A6")),
+    ),
+
+    # FT601 USB3 FIFO clock (100 MHz from FT601)
+    ("usb_fifo_clock", 0, Pins("W19"), IOStandard("LVCMOS33")),
+
+    # FT601 USB3 FIFO interface
+    ("usb_fifo", 0,
+        Subsignal("rst",   Pins("Y9")),
+        Subsignal("data",  Pins(
+            "N13 N14 N15 P15 P16 N17 P17 R17",
+            "P19 R18 R19 T18 U18 V18 V19 V17",
+            "W20 Y19 T21 T20 U21 V20 W22 W21",
+            "Y22 Y21 AA21 AB22 AA20 AB21 AA19 AB20")),
+        Subsignal("be",    Pins("Y18 AA18 AB18 W17")),
+        Subsignal("rxf_n", Pins("AB8")),
+        Subsignal("txe_n", Pins("AA8")),
+        Subsignal("rd_n",  Pins("AA6")),
+        Subsignal("wr_n",  Pins("AB7")),
+        Subsignal("oe_n",  Pins("AB6")),
+        Subsignal("siwua", Pins("Y8")),
+        IOStandard("LVCMOS33"), Misc("SLEW=FAST"),
+    ),
+]
+
+class Platform(XilinxPlatform):
+    default_clk_name   = "clk100"
+    default_clk_period = 1e9/100e6
+
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xc7a35t-fgg484-2", _io, toolchain="vivado")
+        self.toolchain.bitstream_commands = [
+            "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]",
+            "set_property BITSTREAM.CONFIG.CONFIGRATE 40 [current_design]",
+            "set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]",
+        ]
+        self.toolchain.additional_commands = [
+            "write_cfgmem -force -format bin -interface spix4 -size 16 "
+            "-loadbit \"up 0x0 {build_name}.bit\" -file {build_name}.bin"
+        ]
